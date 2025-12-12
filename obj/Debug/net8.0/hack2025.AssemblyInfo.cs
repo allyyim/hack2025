0b28b478bde20d8cf31d082a14fb33a3e2e54afa
Alison Yim <yimalison@microsoft.com>
2025-12-12 02:50:09 -0800

    Fix Azure deployment

--- obj/Debug/net8.0/hack2025.AssemblyInfo.cs
+++ obj/Debug/net8.0/hack2025.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("hack2025")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e81c668f620b43a6db468d0d76be64af0c7f66f")]
 [assembly: System.Reflection.AssemblyProductAttribute("hack2025")]
